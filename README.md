eaa24ad84eb03051ded8ed4c2245e26539853df0
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-01-18 19:36:49 +0530

    Migrate carbon_rover from staging
    Signed-off-by: Sahaj Sarup <sahaj.sarup@linaro.org>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
-# carbon_rover
-DIY Rover using 96Boards Carbon running Zephyr
+# Carbon Rover
+
+The Carbon Rover is a 4 WD Robot that uses 6 ultrasonic sensors and 4 IR sensors for object and edge detection.
+The ultrasonic sensors make sure that the rover doesn't bump into objects in it's path and the IR sensors are used to detect edges so it doesn't fall off.
+
+The Rover is programmed using Zephyr RTOS and heavily relies on its multi-threading functionality to collect sensor data and control the motors at the same time.
+
+Since this is Rev 1. there is no control mechanism and the rover follows a pre-defined path, Rev 2 will possibly include control via Bluetooth.
+
+# Table of Contents
+
+- [1) Hardware](#1-hardware)
+   - [1.1) Hardware requirements](#11-hardware-requirements)
+- [2) Software](#2-software)
+   - [2.1) Build Environment Setup](#21-build-environment-setup)
+- [3) Carbon Rover](#3-carbon-rover)
+   - [3.1) Hardware setup](#31-hardware-setup)
+   - [3.2) Building](#32-building)
+   - [3.3) Video Demonstration](#33-video-demonstration)
+
+# 1) Hardware
+
+## 1.1) Hardware requirements
+
+- [96Boards Ccarbon IE](https://www.96boards.org/product/carbon/)
+- 5v 2A USB Power Bank
+- [6x Ultrasonic Sensors HC-SR04](https://www.robomart.com/buy-4-pin-ultrasonic-sensor-module-arduino-raspberry?search=hcsr04&dsearch=hcsr04)
+- 6x 1KOhm Resistors
+- 6x 2KOhm Resistors
+- [4x IR Edge Detection Sensors](https://www.robomart.com/ir-based-digital-color-sensor-black-white)
+- [1x L9110S Motor Controller](https://www.amazon.com/L9110S-Stepper-Driver-Atomic-Market/dp/B01ACIALJ4/ref=sr_1_1?ie=UTF8&qid=1516181215&sr=8-1&keywords=l9110s)
+- [1x 4WD Car Kit](https://www.amazon.in/Rees52-4-Wheel-Chassis-Encoder-Arduino/dp/B071Z3XTQH/ref=sr_1_6?s=industrial&ie=UTF8&qid=1516181373&sr=1-6&keywords=car+kit)
+- Miscellaneous: Quantity as per required:
+  - Connecting wires
+  - Soldering Kit
+  - Screwdriver Set
+  - Double sided tape
+
+
+# 2) Software
+
+## 2.1 Build Environment Setup
+
+- Follow the official Zephyr documentation to setup build environment
+	- [Linux](http://docs.zephyrproject.org/getting_started/installation_linux.html)
+	- [macOS](http://docs.zephyrproject.org/getting_started/installation_mac.html)
+	- [Windows](http://docs.zephyrproject.org/getting_started/installation_mac.html)
+
+# 3) Carbon Rover
+
+![](https://raw.githubusercontent.com/SeeedDocument/BLE-Carbon/master/img/pinout.png)
+
+## 3.1) Hardware Setup
+
+### Pinout
+
+#### IR Edge Sensor
+
+| IR Sensor Position | Carbon Pin |
+|:------------------:|:----------:|
+| Front Left         | PB12       |
+| Front Right        | PB15       |
+| Back Left          | PB14       |
+| Back Right         | PB13       |
+| +                  | +3v3       |
+| GND                | GND        |
+
+
+#### Ultrasonic Sensors
+
+**All Ultrasonic sensors are connected using a Voltage Divider using a combination of 2kOhm and 1KOhm resistors for the echo pin as the Carbon is limited to 3v3 on the GPIO pins. Please follow [this blog](https://www.96boards.org/blog/zephyr-hcsr04/) for the details.**
+
+![](https://www.96boards.org/assets/images/blog/hcsr04-voltage-divider.svg)
+
+| Ultrasonic Sensor Position | Carbon Pin |
+|:---------------------------|------------|
+| Front Left Trigger         | PC2        |
+| Front Left Echo            | PC4        |
+| Front Center Trigger       | PC3        |
+| Front Center Echo          | PC5        |
+| Front Right Trigger        | PC6        |
+| Front Right Echo           | PC8        |
+| Back Right Trigger         | PC7        |
+| Back Right Echo            | PC9        |
+| Back Center Trigger        | PB6        |
+| Back Center Echo           | PB7        |
+| Back Left Trigger          | PB8        |
+| Back Left Echo             | PB9        |
+| +                          | +5v        |
+| GND                        | GND        |
+
+### Motor Controller
+
+| Motor Controller | Carbon |
+|:----------------:|:------:|
+| A1-A             | PA0    |
+| A1-B             | PA2    |
+| B1-A             | PA3    |
+| B1-B             | PA1    |
+
+## 3.2) Building and Flashing
+
+- Clone This Repository
+  ```$ git clone https://github.com/96boards-projects/carbon_rover```
+- Copy the folder ```rover``` into the root directory of Zephyr source.
+- Build
+  ```shell
+  $ cd /path/to/zephyr/root
+  $ source zephyr-env.sh
+  $ cd rover
+  $ mkdir build; cd build
+  $ cmake -DBOARD=96b_carbon ..
+  $ make
+  ```
+- Connect the micro-USB cable to the USB OTG Carbon port and to your computer. The board should power ON. Force the board into DFU mode by keeping the BOOT0 switch pressed while pressing and releasing the RST switch
+- You should see following confirmation on your Linux host:
+```shell
+$ dmesg
+usb 1-2.1: new full-speed USB device number 14 using xhci_hcd
+usb 1-2.1: New USB device found, idVendor=0483, idProduct=df11
+usb 1-2.1: New USB device strings: Mfr=1, Product=2, SerialNumber=3
+usb 1-2.1: Product: STM32 BOOTLOADER
+usb 1-2.1: Manufacturer: STMicroelectronics
+usb 1-2.1: SerialNumber: 3574364C3034
+```
+- Flash
+```shell
+$ sudo make flash
+```
+- At this moment your rover should come alive.
+
+## 3.3) Video Demonstration
+
+[![Demo](https://img.youtube.com/vi/BEC8CVmJyRc/0.jpg)](https://www.youtube.com/watch?v=BEC8CVmJyRc)
---
+++ rover/src/main.c
@@ -0,0 +1,368 @@
+#include <zephyr.h>
+#include <device.h>
+#include <gpio.h>
+#include <misc/printk.h>
+#include <sys_clock.h>
+#include <misc/util.h>
+#include <string.h>
+
+#define PORT	"GPIOA"
+#define PORT2 "GPIOB"
+#define PORT3 "GPIOC"
+
+// Motor Controller Pins GPIOA
+#define A1A	0
+#define A1B	2
+#define B1A	3
+#define B1B	1
+
+// IR Edge sensor Pins GPIOB
+#define IRFL	12
+#define IRFR	15
+#define IRBL	14
+#define IRBR	13
+
+// Ultrasonic Sensor Pins
+#define TRIGFL	2 //GPIOC
+#define ECHOFL	4
+#define TRIGFC	3 //GPIOC
+#define ECHOFC	5
+#define TRIGFR	6 //GPIOC
+#define ECHOFR	8
+#define TRIGBR	7 //GPIOC
+#define ECHOBR	9
+#define TRIGBC	6 //GPIOB
+#define ECHOBC	7
+#define TRIGBL	8 //GPIOB
+#define ECHOBL	9
+
+#define SLEEP_TIME 1000
+
+#define STACKSIZE 1024
+#define PRIORITY 7
+
+#define DELAY K_MSEC(10)
+
+static K_THREAD_STACK_ARRAY_DEFINE(stacks, 3, STACKSIZE);
+static struct k_thread __kernel threads[3];
+
+static struct device *gpioa;
+static struct device *gpiob;
+static struct device *gpioc;
+
+static uint32_t dir = 0, irfr = 0, irfl = 0, irbr = 0, irbl = 0, us_fl = 0, us_fc = 0, us_fr = 0, us_br = 0, us_bc = 0, us_bl = 0;
+static char tdir[10] = "n";
+
+
+void fwd();
+void bwd();
+void stop();
+void left();
+void right();
+void read_us();
+void read_ir();
+void run();
+uint32_t get_us(uint32_t trig, uint32_t echo, struct device *dev);
+
+void main(void)
+{
+	int ret;
+
+	gpioa = device_get_binding(PORT);
+	if (!gpioa) {
+		printk("Cannot find %s!\n", PORT);
+		return;
+	}
+	gpiob = device_get_binding(PORT2);
+	if (!gpiob) {
+		printk("Cannot find %s!\n", PORT2);
+		return;
+	}
+	gpioc = device_get_binding(PORT3);
+	if (!gpioc) {
+		printk("Cannot find %s!\n", PORT3);
+		return;
+	}
+	//Motor
+	ret = gpio_pin_configure(gpioa, A1A, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PA%d!\n", A1A);
+		return;
+	}
+	ret = gpio_pin_configure(gpioa, A1B, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PA%d!\n", A1B);
+		return;
+	}
+	ret = gpio_pin_configure(gpioa, B1A, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PA%d!\n", B1A);
+		return;
+	}
+	ret = gpio_pin_configure(gpioa, B1B, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PA%d!\n", B1B);
+		return;
+	}
+
+	//IR
+	ret = gpio_pin_configure(gpiob, IRFL, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PB%d!\n", IRFL);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, IRFR, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PB%d!\n", IRFR);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, IRBL, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PB%d!\n", IRBL);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, IRBR, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PB%d!\n", IRBR);
+		return;
+	}
+
+	//US
+	ret = gpio_pin_configure(gpioc, TRIGFL, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PC%d!\n", TRIGFL);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, ECHOFL, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PC%d!\n", ECHOFL);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, TRIGFC, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PC%d!\n", TRIGFC);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, ECHOFC, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PC%d!\n", ECHOFC);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, TRIGFR, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PC%d!\n", TRIGFR);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, ECHOFR, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PC%d!\n", ECHOFR);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, TRIGBR, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PC%d!\n", TRIGBR);
+		return;
+	}
+	ret = gpio_pin_configure(gpioc, ECHOBR, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PC%d!\n", ECHOBR);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, TRIGBC, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PB%d!\n", TRIGBC);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, ECHOBC, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PB%d!\n", ECHOBC);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, TRIGBL, GPIO_DIR_OUT);
+	if (ret) {
+		printk("Error configuring PB%d!\n", TRIGBL);
+		return;
+	}
+	ret = gpio_pin_configure(gpiob, ECHOBL, GPIO_DIR_IN);
+	if (ret) {
+		printk("Error configuring PB%d!\n", ECHOBL);
+		return;
+	}
+
+	//Start threads
+	k_thread_create(&threads[0], &stacks[0][0], STACKSIZE, read_us, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
+	k_thread_create(&threads[1], &stacks[1][0], STACKSIZE, read_ir, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
+	k_thread_create(&threads[2], &stacks[2][0], STACKSIZE, run, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
+}
+
+void bwd()
+{
+	gpio_pin_write(gpioa, A1A, 1);
+	gpio_pin_write(gpioa, A1B, 0);
+	gpio_pin_write(gpioa, B1A, 1);
+	gpio_pin_write(gpioa, B1B, 0);
+	strcpy(tdir, "forward");
+}
+
+void fwd()
+{
+	gpio_pin_write(gpioa, A1A, 0);
+	gpio_pin_write(gpioa, A1B, 1);
+	gpio_pin_write(gpioa, B1A, 0);
+	gpio_pin_write(gpioa, B1B, 1);
+	strcpy(tdir, "backward");
+}
+
+void stop()
+{
+	gpio_pin_write(gpioa, A1A, 0);
+	gpio_pin_write(gpioa, A1B, 0);
+	gpio_pin_write(gpioa, B1A, 0);
+	gpio_pin_write(gpioa, B1B, 0);
+	strcpy(tdir, "STOP");
+}
+
+void left() {
+	gpio_pin_write(gpioa, A1A, 1);
+	gpio_pin_write(gpioa, A1B, 0);
+	gpio_pin_write(gpioa, B1A, 0);
+	gpio_pin_write(gpioa, B1B, 1);
+	strcpy(tdir, "left spin");
+}
+
+void right() {
+	gpio_pin_write(gpioa, A1A, 0);
+	gpio_pin_write(gpioa, A1B, 1);
+	gpio_pin_write(gpioa, B1A, 1);
+	gpio_pin_write(gpioa, B1B, 0);
+	strcpy(tdir, "right spin");
+}
+
+uint32_t get_us(uint32_t trig, uint32_t echo, struct device *dev)
+{
+	uint32_t cycles_spent;
+	uint32_t nanseconds_spent;
+	uint32_t val;
+	uint32_t cm;
+	uint32_t stop_time;
+	uint32_t start_time;
+	gpio_pin_write(dev, trig, 1);
+	k_sleep(K_MSEC(10));
+	gpio_pin_write(dev, trig, 0);
+	start_time = k_cycle_get_32();
+	do {
+		gpio_pin_read(dev, echo, &val);
+		stop_time = k_cycle_get_32();
+		cycles_spent = stop_time - start_time;
+		if (cycles_spent > 243600) //50cm for 84MHz (((MAX_RANGE * 58000) / 1000000000) * (CLOCK * 1000000))
+		{
+			return 50;
+		}
+	} while (val == 0);
+	start_time = k_cycle_get_32();
+
+	do {
+		gpio_pin_read(dev, echo, &val);
+		stop_time = k_cycle_get_32();
+		cycles_spent = stop_time - start_time;
+	} while (val == 1);
+	nanseconds_spent = SYS_CLOCK_HW_CYCLES_TO_NS(cycles_spent);
+	cm = nanseconds_spent / 58000;
+	//printk("%d\n", cm);
+	return cm;
+	k_sleep(DELAY);
+}
+
+void read_ir()
+{
+	while(1)
+	{
+		gpio_pin_read(gpiob, IRFR, &irfr);
+		gpio_pin_read(gpiob, IRFL, &irfl);
+		gpio_pin_read(gpiob, IRBR, &irbr);
+		gpio_pin_read(gpiob, IRBL, &irbl);
+		k_sleep(DELAY);
+	}
+}
+
+void read_us()
+{
+	while(1)
+	{
+		us_fl = get_us(TRIGFL, ECHOFL, gpioc);
+		us_fc = get_us(TRIGFC, ECHOFC, gpioc);
+		us_fr = get_us(TRIGFR, ECHOFR, gpioc);
+		us_br = get_us(TRIGBR, ECHOBR, gpioc);
+		us_bc = get_us(TRIGBC, ECHOBC, gpiob);
+		us_bl = get_us(TRIGBL, ECHOBL, gpiob);
+		k_sleep(DELAY);
+	}
+}
+
+void uart_out()
+{
+		printk("Front Left US: %d\tFront Center US: %d\tFront Right US: %d\nBack Right US: %d\tBack Center US: %d\tBack Letf US: %d\nFront Right IR: %d\tFront Left IR: %d\nBack Right IR: %d\tBack Letf IR: %d\nDirection: %s\n", us_fl, us_fc, us_fr, us_br, us_bc, us_bl, irfr, irfl, irbr, irbl, tdir);
+}
+
+void run()
+{
+	while (1) {
+
+		if((irfr == 0 && irfl == 0 && irbr == 0 && irbl == 0) || (us_fc < 15 && us_bc < 15))
+		{
+			stop();
+			dir = 0;
+		}
+
+		else if(dir == 0)
+		{
+			if((irfr == 0 && irfl == 1) || (us_fr < 15 && us_fl > 15))
+			{
+				left();
+			}
+
+			else if((irfl == 0 && irfr == 1) || (us_fr > 15 && us_fl < 15))
+			{
+				right();
+			}
+
+			else if (irfr == 1 && irfl == 1 && us_fr > 15 && us_fl > 15 && us_fc > 15)
+			{
+				fwd();
+			}
+
+			else if((irfr == 0 && irfl == 0) || (us_fc < 15))
+			{
+				bwd();
+				dir = 1;
+			}
+		}
+
+		else if(dir == 1)
+		{
+			if((irbr == 0 && irbl == 1) || (us_br < 15 && us_bl > 15))
+			{
+				right();
+			}
+
+			else if((irbl == 0 && irbr == 1) || (us_br > 15 && us_bl < 15))
+			{
+				left();
+			}
+
+			else if(irbr == 1 && irbl == 1 && us_br > 15 && us_bl > 15 && us_bc > 15)
+			{
+				bwd();
+			}
+
+			else if((irbr == 0 && irbr == 0) || (us_bc < 15))
+			{
+				fwd();
+				dir = 0;
+			}
+		}
+		uart_out();
+
+		k_sleep(DELAY);
+	}
+}
